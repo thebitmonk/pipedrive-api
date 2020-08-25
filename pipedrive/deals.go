174637474932ea02853a798da475215d009ea6d2
thebitmonk <varun@sendx.io>
2020-08-26 03:26:39 +0530

    Fixing deals API as per latest PipeDrive API Spec

--- pipedrive/deals.go
+++ pipedrive/deals.go
@@ -391,22 +391,16 @@ type DealCreateOptions struct {
 // Pipedrive API docs: https://developers.pipedrive.com/docs/api/v1/#!/Deals/post_deals
 func (s *DealService) Create(ctx context.Context, opt *DealCreateOptions) (*DealResponse, *Response, error) {
 	req, err := s.client.NewRequest(http.MethodPost, "/deals", nil, struct {
-		Title               string    `json:"title"`
-		Value               string    `json:"value"`
-		Currency            string    `json:"currency"`
-		UserID              uint      `json:"user_id"`
-		PersonID            uint      `json:"person_id"`
-		OrgID               uint      `json:"org_id"`
-		StageID             uint      `json:"stage_id"`
-		Status              string    `json:"status"`
-		Probability         uint      `json:"probability"`
-		LostReason          string    `json:"lost_reason"`
-		AddTime             string    `json:"add_time"`
-		VisibleTo           VisibleTo `json:"visible_to"`
-		RequirementAnalysis string    `json:"56d3d40c37c0db60fff576ae73ba2fea0d58dc09"`
-		WantedStartTime     string    `json:"a3114acce61bb930180af173b395d76f42af8794"`
-		TemporaryLink       string    `json:"4fe88fad67d8dcbc17d18d9ee1faac55122249fd,omitempty"`
-		LeadSource          uint      `json:"5d4fbabc9b032aeb3df515d9c66994d6892ee062,omitempty"`
+		Title       string `json:"title"`
+		Value       string `json:"value"`
+		Currency    string `json:"currency"`
+		UserID      uint   `json:"user_id"`
+		PersonID    uint   `json:"person_id"`
+		OrgID       uint   `json:"org_id"`
+		StageID     uint   `json:"stage_id"`
+		Status      string `json:"status"`
+		Probability uint   `json:"probability"`
+		LostReason  string `json:"lost_reason"`
 	}{
 		opt.Title,
 		opt.Value,
@@ -418,12 +412,6 @@ func (s *DealService) Create(ctx context.Context, opt *DealCreateOptions) (*Deal
 		opt.Status,
 		opt.Probability,
 		opt.LostReason,
-		opt.AddTime.FormatFull(),
-		opt.VisibleTo,
-		opt.RequirementAnalysis,
-		opt.WantedStartTime.Format(),
-		opt.TemporaryLink,
-		opt.LeadSource,
 	})
 
 	if err != nil {
